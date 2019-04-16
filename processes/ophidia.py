4f112fdf0cfa1b29e78e0e8e5f81dffb81993045
Cosimo Palazzo <cosimo.palazzo@cmcc.it>
2019-04-16 14:34:56 +0200

    First stable version

--- processes/ophidia.py
+++ processes/ophidia.py
@@ -17,14 +17,12 @@
 #
 
 from pywps import Process, LiteralInput, ComplexInput, LiteralOutput, ComplexOutput, Format
-from pywps.app.Common import Metadata
-from pywps.validator.mode import MODE
-from pywps.inout.formats import FORMATS
-
-import logging
-import subprocess
-import StringIO
 from PyOphidia import client as _client, ophsubmit as _ophsubmit
+
+import subprocess
+import logging
+
+LOGGER = logging.getLogger("PYWPS")
 
 _host = "127.0.0.1"
 _port = 11732
@@ -41,34 +39,33 @@ class OphExecuteMain(Process):
             'userid',
             'Username',
             abstract="User identifier for Ophidia system",
-            data_type=type(''))
+            data_type='string')
 
         passwd = LiteralInput(
             'passwd',
             'Password',
             abstract="Password to access Ophidia",
-            data_type=type(''))
+            data_type='string')
 
         request = ComplexInput(
             'request',
             'JSON Request',
-            supported_formats=[FORMATS('text/json', encoding='base64'), FORMATS('text/plain', encoding='utf-8')])
-        # maxmegabites=1 ?-> max_occurrence=1
+            supported_formats=[Format('text/json', encoding='base64'), Format('text/plain', encoding='utf-8')])
 
         jobid = LiteralOutput(
             'jobid',
             'Ophidia JobID',
-            data_type=type(''))
+            data_type='string')
 
         response = ComplexOutput(
             'response',
             'JSON Response',
-            supported_formats=[FORMATS('text/json', encoding='base64'), FORMATS('text/plain', encoding='utf-8')])
+            supported_formats=[Format('text/json', encoding='base64'), Format('text/plain', encoding='utf-8')])
 
         error = LiteralOutput(
             'return',
             'Return code',
-            data_type=type(1))
+            data_type='integer')
 
         inputs = [userid, passwd, request]
         outputs = [jobid, response, error]
@@ -81,52 +78,44 @@ class OphExecuteMain(Process):
             version=_version,
             inputs=inputs,
             outputs=outputs,
-            store_supported=True,  # or False??
-            status_supported=True  # or False??
+            store_supported=True,
+            status_supported=True
         )
 
     def _handler(self, request, response):
 
-        self.status.set("Pre-processing", 1)
-        logging.debug("Incoming a request with format %s" % self.request.format)
+        response.update_status("Pre-processing", 1)
 
-        request.outputs['return'][0].file = 1
-        request.outputs['jobid'][0].file = ""
+        LOGGER.debug("Incoming a request with format %s" % request.inputs['request'][0].data_format.encoding)
 
-        logging.debug("Building request for oph_server")
-        file = open(request.inputs['request'][0].file, 'r')
+        response.outputs['jobid'].data = ""
+        response.outputs['response'].data = ""
+        response.outputs['return'].data = 1
 
-        buffer = file.read()
-        file.close()
+        buffer = request.inputs['request'][0].data
 
-        if request.inputs['request'][0].data_format == "base64":
-            logging.debug("Decoding request")
-            buffer = buffer.decode("base64")
+        LOGGER.debug("Request: %s" % buffer)
 
-        logging.debug("Request: %s" % buffer)
+        response.update_status("Running", 2)
 
-        self.status.set("Running", 2)
-        logging.debug("Execute the job")
-        buffer, request.outputs['jobid'][0].file, new_session, return_value, request.outputs['return'][0].file = 1 = _ophsubmit.submit(request.inputs['userid'][0].file, request.inputs['passwd'][0].file, _host, _port, buffer)
+        LOGGER.debug("Execute the job")
 
-        logging.debug("Return value: %s" % return_value)
-        logging.debug("JobID: %s" % jobid)
-        logging.debug("Response: %s" % buffer)
-        logging.debug("Error message: %s" % error)
+        buffer, jobid, new_session, return_value, error = _ophsubmit.submit(request.inputs['userid'][0].data, request.inputs['passwd'][0].data, _host, _port, buffer)
 
-        self.status.set("Post-processing", 98)
-        if return_value == 0 and len(buffer) > 0 and request.inputs['request'][0].data_format == "base64":
-            logging.debug("Encoding response")
-            buffer = buffer.encode("base64")
+        LOGGER.debug("Return value: %s" % return_value)
+        LOGGER.debug("JobID: %s" % jobid)
+        LOGGER.debug("Response: %s" % buffer)
+        LOGGER.debug("Error message: %s" % error)
 
-        self.status.set("Outputting", 99)
-        output = StringIO.StringIO()
-        response.outputs['error'].file = return_value
+        response.update_status("Post-processing", 99)
+
+        response.outputs['return'].data = return_value
         if return_value == 0:
             if jobid is not None:
-                response.outputs['jobid'].file = jobid
+                response.outputs['jobid'].data = jobid
             if len(buffer) > 0:
-                output.write(buffer)
-        response.outputs['response'].file = output
+                response.outputs['response'].data = buffer
 
-        self.status.set("Succeded", 100)
+        response.update_status("Succeded", 100)
+
+        return response
